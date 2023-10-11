5a32104d1c80cd4ad65e2d80c584d145db8b4717
nabuserrieh <nabuserrieh@aspire-infotech.net>
2023-10-11 15:21:38 +0300

    Updating TC to check that response status of trackers is 200

--- tests/sandboxAnchorTest.spec.js
+++ tests/sandboxAnchorTest.spec.js
@@ -44,21 +44,21 @@ test.describe('Sandbox Anchor Ad Page', () => {
       // Start waiting for new page before clicking
       const pagePromise = context.waitForEvent('page');
 
-      // Log 'request' and 'response' events.
-      page.on('request', request => console.log('>>', request.method(), request.url()));
-      page.on('response', response => console.log('<<', response.status(), response.url()));
+      // Log 'request' and 'response' events. // un-commnet if you want it check them in console
+      // page.on('request', request => console.log('>>', request.method(), request.url()));
+      // page.on('response', response => console.log('<<', response.status(), response.url()));
 
       // defining trackers(this needs to be enhanced to find better way to define trackers using a helper)
-        const responseRequestPromise = page.waitForRequest(request => request.url().match('imp_track-response'));
-        const serveRequestPromise = page.waitForRequest(request => request.url().match('imp_track-serve'));
-        const impressionRequestPromise = page.waitForRequest(request => request.url().match('impression'));
-        const viewCompleteRequestPromise = page.waitForRequest(request => request.url().match('imp_track-completeview'));
-        const viewRequestPromise = page.waitForRequest(request => request.url().match('imp_track-view'));
-        const cacheBusterRequestPromise = page.waitForRequest(request => request.url().match('%%CACHEBUSTER%%'));
-        const clickWebRequestPromise = page.waitForRequest(request => request.url().match('clickUrl=https'));
-        const krakenBillableRequestPromise = page.waitForRequest(request => request.url().match('event/billable'));
-        const clickRequestPromise = page.waitForRequest(request => request.url().match('imp_track-click'));
-        const closeRequestPromise = page.waitForRequest(request => request.url().match('imp_track-close'));
+        const responseRequestPromise = page.waitForResponse(request => request.url().match('imp_track-response'));
+        const serveRequestPromise = page.waitForResponse(request => request.url().match('imp_track-serve'));
+        const impressionRequestPromise = page.waitForResponse(request => request.url().match('impression'));
+        const viewCompleteRequestPromise = page.waitForResponse(request => request.url().match('imp_track-completeview'));
+        const viewRequestPromise = page.waitForResponse(request => request.url().match('imp_track-view'));
+        const cacheBusterRequestPromise = page.waitForResponse(request => request.url().match('%%CACHEBUSTER%%'));
+        const clickWebRequestPromise = page.waitForResponse(request => request.url().match('clickUrl=https'));
+        const krakenBillableRequestPromise = page.waitForResponse(request => request.url().match('event/billable'));
+        const clickRequestPromise = page.waitForResponse(request => request.url().match('imp_track-click'));
+        const closeRequestPromise = page.waitForResponse(request => request.url().match('imp_track-close'));
         // const moatRequestPromise = page.waitForResponse(request => request.status() === 400);
 
        // Go to the starting url
@@ -70,7 +70,7 @@ test.describe('Sandbox Anchor Ad Page', () => {
        await expect(adFrame.locator('.celtra-screen-holder')).toBeVisible(); // Check that ad is visible
 
        // wait for trackers
-       const waitATrackersrray = await Promise.all([
+       const waitTrackersArray = await Promise.all([
         responseRequestPromise,
         serveRequestPromise,
         impressionRequestPromise,
@@ -81,12 +81,18 @@ test.describe('Sandbox Anchor Ad Page', () => {
         krakenBillableRequestPromise,
        ]);
 
-       await expect(waitATrackersrray.forEach).toBeTruthy(); // check that each tracker has truthy value (is found in network)
+       await expect(waitTrackersArray.forEach).toBeTruthy(); // check that each tracker has truthy value (is found in network)
 
        // Verify that the first 5 trackers include uuid in the request URL
        for (let i=0; i< 5; i++){
-        await expect(waitATrackersrray[i]._initializer.url).toContain('uuid');
-        console.log('Trackers URL: ', waitATrackersrray[i]._initializer.url);
+        await expect(waitTrackersArray[i]._initializer.url).toContain('uuid');
+        console.log('Tracker URL: ', waitTrackersArray[i]._initializer.url);
+    }
+
+      // Verify that tthe first 7 trackers response status is 200
+      for (let i=0; i< 7; i++){
+      await expect(waitTrackersArray[i].status()).toEqual(200);
+      console.log('Response URL: ', waitTrackersArray[i]._initializer.url);
     }
  
        await adFrame.locator('.celtra-screen-holder').click(); // Click on ad
@@ -97,12 +103,16 @@ test.describe('Sandbox Anchor Ad Page', () => {
   
       const clickRequest = await clickRequestPromise;
       await expect(clickRequest).toBeTruthy();
+      // check tracker url includes uuid & response status is 200
       await expect(clickRequest._initializer.url).toContain('uuid');
+      await expect(clickRequest.status()).toEqual(200);
       await newPage.close();
   
       await page.locator('.filled .kargo-svg-close').click(); // Close the ad
       const closeRequest = await closeRequestPromise;
       await expect(closeRequest).toBeTruthy();
+      // check tracker url includes uuid & response status is 200
       await expect(closeRequest._initializer.url).toContain('uuid');
+      await expect(closeRequest.status()).toEqual(200);
     });
 });
