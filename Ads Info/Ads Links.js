295b6d0b4d2f9acc84e59fa4bb2e26d80e1bfebc
nabuserrieh <nabuserrieh@aspire-infotech.net>
2023-10-16 14:17:00 +0300

    add Anchor ad links

--- Ads Info/Ads Links.js
+++ Ads Info/Ads Links.js
@@ -1,3 +1,5 @@
 export const kargoLink = 'https://www.kargo.com/privacy';
 export const hoverDemoAdLink = 'https://ad-tag-sandbox.staging.kargo.com/ad-tag-test/render.php?krg_debug=4&id=hover-v7';
 export const hoverAdLink = /www.windstarcruises.com/;
+export const anchorDemoAdLink = 'https://ad-tag-sandbox.staging.kargo.com/ad-tag-test/render.php?krg_debug=4&id=anchor-v7';
+export const anchorAdLink = 'https://track.celtra.com/';
