50ee9e361dd627213c142af012136737b0b30c7f
nabuserrieh <nabuserrieh@aspire-infotech.net>
2023-10-17 13:14:34 +0300

    Adding sandboxBreakoutTest.spec.js TC file

--- Ads Info/Ads Links.js
+++ Ads Info/Ads Links.js
@@ -3,3 +3,5 @@ export const hoverDemoAdLink = 'https://ad-tag-sandbox.staging.kargo.com/ad-tag-
 export const hoverAdLink = /www.windstarcruises.com/;
 export const anchorDemoAdLink = 'https://ad-tag-sandbox.staging.kargo.com/ad-tag-test/render.php?krg_debug=4&id=anchor-v7';
 export const anchorAdLink = 'https://track.celtra.com/';
+export const breakoutDemoAdLink = 'https://ad-tag-sandbox.staging.kargo.com/ad-tag-test/render.php?krg_debug=4&id=breakout-v7';
+export const breakoutAdLink = /www.hersheyland.com/;
